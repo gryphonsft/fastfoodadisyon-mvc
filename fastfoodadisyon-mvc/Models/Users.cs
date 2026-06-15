c6a5389ccdc69b90ddb52064200dd3f44dce634c
gryphonsft <abdbzd01@gmail.com>
2026-06-15 17:44:46 +0300

    Proje dosyası ekle.

---
+++ fastfoodadisyon-mvc/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace fastfoodadisyon_mvc.Models
+{
+    public sealed class Users
+    {
+        public int Id { get; set; }
+        public string username { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
